8c39e21f46fa9fddeb8eb0a15c6910e0efa5029a
Wafaaessa <Wafaaessa98@icloud.com>
2024-01-13 23:58:58 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["","Designer","Developer", "Photographer","Freelancer"],
+    typeSpeed: 120,
+    backSpeed: 50,
+    loop: true,
+    smartBackspace: true // Default value
+  });
